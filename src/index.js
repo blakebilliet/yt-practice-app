b8ffd7b5780a17f910260ed34f6d60fd58c49c3b
blakebilliet <billiet@ccs.neu.edu>
2018-02-04 11:52:39 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create a new component. This component creates some HTML
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+const API_KEY = 'AIzaSyCoqT12cbazCDD1ckeHUF5KdWIx9Sa2lus';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    constructor(props) {
+        super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('surfboards');
+
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
+        });
+    }
+
+    render() {
+
+        const videoSearch= _.debounce((term) => {this.videoSearch(term)}, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}
+                />
+            </div>
+        );
+    }
+
+}
+
+// Add components generated HTML and put it in the DOM
+ReactDom.render(<App/>, document.querySelector('.container'));
